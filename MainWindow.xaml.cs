a8bd0e0978928f3d0c1b779f2e825de4ee33b00c
anistirut <turitsinaliza12@mail.ru>
2025-09-11 13:31:59 +0500

    Создание таймера и метода AttackPlayer. Турицина.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using WpfApp2.Glasses;
 
 namespace WpfApp2
@@ -22,11 +23,21 @@ namespace WpfApp2
     public partial class MainWindow : Window
     {
         public Glasses.PersonInfo Player = new Glasses.PersonInfo("Student", 100, 10, 1, 0, 0, 5);
+        public List<PersonInfo> Enemys = new List<PersonInfo>();
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
-            //Повышаем уровень персонажа и обновляем данные на UI
             UserInfoPlayer();
+
+            Enemys.Add(new PersonInfo("Маленький монстр", 100, 10, 1, 0, 15, 10));
+            Enemys.Add(new PersonInfo("Средний монстр", 120, 15, 1, 0, 30, 15));
+            Enemys.Add(new PersonInfo("Большой монстр", 150, 20, 1, 0, 40, 20));
+
+            dispatcherTimer.Tick += AtackPlayer;
+            dispatcherTimer.Interval = new System.TimeSpan(0, 0, 10);
+            dispatcherTimer.Start();
+
         }
         ///<summary> Повышение уровня и обновляемых данных
         public void UserInfoPlayer()
@@ -44,6 +55,11 @@ namespace WpfApp2
             playerLevel.Content = "Уровень: " + Player.Level;
             playerGlasses.Content = "Опыт: " + Player.Glasses;
             playerMoney.Content = "Монеты: " + Player.Money;
+        }
+
+        private void AttackEnemy(object sender, MouseButtonEventArgs e)
+        {
+
         }
     }
 }
